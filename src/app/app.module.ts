82ff4ab8bb208cd6d5574e69e826e355dbab8040
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-03-25 10:24:47 -0300

    feat: 35. dividindo o app em vários componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentRegistryComponent } from './student-registry/student-registry.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentRegistryComponent,
+    StudentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
