5c57d0e6359bef1f440e3f5343b9648a06a94587
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2022-02-22 06:03:48 -0300

    repositório limpo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   students = [];
+  newStudentName = '';
+  newStudentDescription = '';
+
+  addApproved() {
+    this.students.push({
+      type: 'approved',
+      name: this.newStudentName,
+      description: this.newStudentDescription
+    })
+  }
+
+  addSubstitute() {
+    this.students.push({
+      type: 'substitute',
+      name: this.newStudentName,
+      description: this.newStudentDescription
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StudentRegistryComponent } from './student-registry/student-registry.component';
-import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StudentRegistryComponent,
-    StudentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
